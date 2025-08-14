7da0d21483f63d446ef393728133b7551972dfbf
Jeremy Watt <jeremywatt@Jeremys-Mac-mini.local>
2025-08-14 16:13:08 -0700

    task 1.2 complete

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    "@tailwindcss/postcss",
+    "@tailwindcss/typography",
+    "@tailwindcss/forms",
+  ],
 };
 
 export default config;
